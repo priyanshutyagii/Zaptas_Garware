800995c87e2e4b9b9724d33c260b1ed5dd784f82
Zaptas Technologies Pvt Ltd <kushagrakamal98@gmail.com>
2024-12-19 16:00:12 +0530

    Update connect.js

--- src/config/connect.js
+++ src/config/connect.js
@@ -1,8 +1,10 @@
 const ConnectMe = {
-  //  BASE_URL: "https://intranet.zaptas.in/v1",
+   BASE_URL: "https://intranet.zaptas.in/v1",
    img_URL: "https://intranet.zaptas.in",
   // img_URL: "http://localhost:3060",
-  BASE_URL: 'http://localhost:3060/v1'
+  // BASE_URL: 'http://localhost:3060/v1'
+  // BASE_URL: "http://192.168.3.108/v1",
+  // img_URL: "http://192.168.3.108",
 };
 
 export default ConnectMe;
