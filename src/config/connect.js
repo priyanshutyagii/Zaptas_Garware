80fc996a52e8e03524d6cb3df5a5f3ba0aff6650
Anil Rai <anil.rai31789@gmail.com>
2025-01-01 15:18:58 +0530

    css fixed

--- src/config/connect.js
+++ src/config/connect.js
@@ -1,8 +1,8 @@
 const ConnectMe = {
-  //  BASE_URL: "https://intranet.zaptas.in/v1",
-   img_URL: "https://intranet.zaptas.in",
+  BASE_URL: "https://intranet.zaptas.in/v1",
+  img_URL: "https://intranet.zaptas.in",
   // img_URL: "http://localhost:3060",
-  BASE_URL: 'http://localhost:3060/v1'
+  // BASE_URL: 'http://localhost:3060/v1'
   // BASE_URL: "http://192.168.3.108/v1",
   // img_URL: "http://192.168.3.108",
 };
